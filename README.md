45418c695bdda1d7055d15482c80f0435382b2ca
bradmurphy <bm.dev84@gmail.com>
2014-08-18 23:28:13 -0400

    Final updates

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Android vs Apple
+Square One
 ==========
 
-Product battle concept with CSS3 Animation and Scroll Events.
+My personal framework/workflow using Bourbon, HTML5 Boilerplate and Grunt.
+
+Updates imminent:
+
+* 06/25/14: Added Bitters & updates to the index page.
+* 06/26/14: Configured Gruntfile.js for workflow including concat, image minify, uglify, sass, watch (with livereload).
+* 06/27/14: Added HTML watch task for live reload and package.json with dependencies for grunt.
+* 07/05/14: Added js-hint and put normalize.css in SASS assets.
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,67 @@ var battleAPI = (function () {
 
 	// VS ANIMATION
 
-	function vsAnimation() {
+	function animations() {
 
 		setTimeout(function() {
 			$("#vs").attr("class", "vs colors");
 		}, 1900);
+
+		// SPECS HEADER ANIMATIONS
+		$('h3').waypoint(function() {
+		  $(this).attr("class", "animated bounceIn");
+		}, { offset: "75%" });
+
+		$('#androidLogo').waypoint(function() {
+		  $(this).attr("class", "animated bounceIn");
+		}, { offset: "75%" });
+
+		$('#appleLogo').waypoint(function() {
+		  $(this).attr("class", "animated bounceIn");
+		}, { offset: "75%" });
+
+		// SPECS ANIMATIONS
+		$("#display").waypoint(function() {
+		  $(this).attr("class", "specWrap flipIn");
+		  $("#displayX").attr("class", "fa fa-times scaleIn");
+		  $("#displayCheck").attr("class", "fa fa-check scaleIn");
+
+		  $("#custom").attr("class", "specWrap flipIn");
+		  $("#customCheck").attr("class", "fa fa-check scaleIn");
+		  $("#customX").attr("class", "fa fa-times scaleIn");
+
+		  $("#battery").attr("class", "specWrap flipIn");
+		  $("#batteryCheck").attr("class", "fa fa-check scaleIn");
+		  $("#batteryX").attr("class", "fa fa-times scaleIn");
+
+		  $("#camera").attr("class", "specWrap flipIn");
+		  $("#cameraCheck1").attr("class", "fa fa-check scaleIn");
+		  $("#cameraCheck2").attr("class", "fa fa-check scaleIn");
+
+		  $("#os").attr("class", "specWrap flipIn");
+		  $("#osCheck").attr("class", "fa fa-check scaleIn");
+		  $("#osX").attr("class", "fa fa-times scaleIn");
+
+		  $("#price").attr("class", "specWrap flipIn");
+		  $("#priceCheck").attr("class", "fa fa-check scaleIn");
+		  $("#priceX").attr("class", "fa fa-times scaleIn");
+
+		}, { offset: "75%" });
+
+		// BIG SEXY ANIMATION
+		$('#bigSexy').waypoint(function() {
+		  	$(this).attr("class", "dropDown");
+		  	$("h6").attr("class", "animated zoomIn");
+		}, { offset: "75%" });
+
+		$('#bigSexyCenter').waypoint(function() {
+		  	$("#bigSexyLeft").attr("class", "leftAnimation");
+		  	$("#bigSexyCenter").attr("class", "dropDownCenter");
+		  	$("#bigSexyRight").attr("class", "rightAnimation");
+		  	$("h6").attr("class", "animated zoomIn");
+		}, { offset: "75%" });
+
+
 
 	}
 
@@ -16,7 +72,7 @@ var battleAPI = (function () {
 	function init() {
 
 		// CALL INIT FUNCTIONS
-		vsAnimation();
+		animations();
 
 	}
 
